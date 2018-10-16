6f1a3aa3af7f983002f69e3a09e9275ee436689e
Bradley Low <bradleylow93@gmail.com>
2018-10-16 12:41:29 -0700

    Added routing for App, included routing for Options component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
+import Options from './containers/Options/Options';
 
 class App extends Component {
 
     render() {
-        let routes = null;
-
         return (
             <Layout>
-                {routes}
+                <Switch>
+                    <Route path="/" exact component={Options} />
+                </Switch>
             </Layout>
         );
     }
