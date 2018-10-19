9aafec2951e36136d63d7838d4d13f9cc6e701f3
Bradley Low <bradleylow93@gmail.com>
2018-10-18 20:14:19 -0700

    Added routing to results page.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import Options from './containers/Options/Options';
 import Scoreboard from './containers/Scoreboard/Scoreboard';
+import Result from './containers/Result/Result';
 
 class App extends Component {
 
@@ -12,6 +13,7 @@ class App extends Component {
             <Layout>
                 <Switch>
                     <Route path="/scoreboard" exact component={Scoreboard} />
+                    <Route path="/results" exact component={Result} />
                     <Route path="/" exact component={Options} />
                 </Switch>
             </Layout>
