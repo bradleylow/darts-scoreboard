a14c615aab8caeacad7505d475edb151edf25f70
Bradley Low <bradleylow93@gmail.com>
2018-10-18 22:28:45 -0700

    Removed import of withRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import Options from './containers/Options/Options';
