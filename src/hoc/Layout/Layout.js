6160931a0f0649cdb3b99cdc6c6c99c1ab31ab80
Bradley Low <bradleylow93@gmail.com>
2018-10-19 14:36:49 -0700

    Style bug fix for desktop sized scoreboard.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -7,7 +7,7 @@ class Layout extends Component {
     render () {
         return (
             <Aux>
-                <main className="contain mx-auto px-8 lg:px-0">
+                <main className="contain mx-auto relative px-8 lg:px-0 sm:h-screen">
                     {this.props.children}
                 </main>
             </Aux>
